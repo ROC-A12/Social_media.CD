cde4db2a7f47efa525ef6698cdccef01ae28bbbf
Cas1107 <410007301@st.roc.a12.nl>
2026-01-13 13:36:32 +0100

    test

--- follow.php
+++ follow.php
@@ -10,7 +10,7 @@ $user_id = $_SESSION['user_id'];
 $follow_user_id = (int)$_GET['user_id'];
 
 if ($follow_user_id != $user_id) {
-    // Check if already following
+    // Check if already following.
     $check_stmt = $db->prepare("SELECT id FROM followers WHERE follower_id = ? AND following_id = ?");
     $check_stmt->bind_param("ii", $user_id, $follow_user_id);
     $check_stmt->execute();
